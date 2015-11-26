f75ea58daaedeec599b58a87e4349e49054ed82e
joanna-chen <joannachen.x@gmail.com>
2015-11-26 12:53:38 -0500

    initial commit

---
+++ EasyApp/EasyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyApp
+//
+//  Created by Joanna Chen on 2015-11-26.
+//  Copyright © 2015 Joanna Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
